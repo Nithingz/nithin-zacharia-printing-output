a90b0b9eb3c3b4597b57044b06c9ba14aa414820
Nithingz <nithingz@gmail.com>
2020-06-01 13:33:44 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,39 @@
+let a = 3;
+let b = 5;
+let c;
+
+
+let outpot = 'let a = 3\n';
+outpot += 'let b = 5\n';
+outpot += 'let c\n';
+outpot += '-----------\n';
+
+outpot += 'a + b =' +(a+b)+'\n';
+outpot += 'a - b =' +(a-b)+'\n';
+outpot += 'a * b =' +(a*b)+'\n';
+outpot += 'a / b =' +(a/b)+'\n';
+outpot += 'a % b =' +(a%b)+'\n';
+outpot += 'a += b =' +(a+=b)+'\n';
+outpot += 'a -= b =' +(a-=b)+'\n';
+outpot += 'a /= b =' +(a/=b)+'\n';
+outpot += 'a %= b =' +(a%=b)+'\n';
+outpot += 'a == b =' +(a==b)+'\n';
+outpot += 'a! = b =' +(a!=b)+'\n';
+outpot += 'a > b =' +(a>b)+'\n';
+outpot += 'a < b =' +(a<b)+'\n';
+outpot += '!a && !c =' +(!a&&!c)+'\n';
+outpot += '!a || !c =' +(!a || !c);
+
+
+
+alert( outpot );
+
+
+let first_name = 'Nithin ';
+let last_name = 'Zacharia';
+let email = 'nithingz@gmail.com';
+let output;
+output = "My name is " + first_name + last_name + ". You can contact me at " + email;
+alert(output);
+
+
